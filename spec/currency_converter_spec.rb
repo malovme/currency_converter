55cc50ecb082d2bf63ee634b1281c94334193eda
Michael Malov <mmeemail@gmail.com>
2017-07-05 21:17:01 +0300

    write spec descriptions for future Money class

--- spec/currency_converter_spec.rb
+++ spec/currency_converter_spec.rb
@@ -4,8 +4,42 @@ RSpec.describe CurrencyConverter do
   it "has a version number" do
     expect(CurrencyConverter::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+RSpec.describe CurrencyConverter::Money do
+  it "should set base currency while setting conversion rates"
+  it "should set conversion rates while setting conversion rates"
+
+  it "should be valid with integer amount and string currency"
+  it "should be valid with float amount and symbol currency"
+
+  it "should be invalid without amount"
+  it "should be invalid without currency"
+
+  it "should return amount"
+  it "should return currency"
+  it "should return inspect"
+
+  it "should convert one currency to another (return Money class object)"
+  it "should convert one currency to another (check amount)"
+
+  it "should add moneys with same currency"
+  it "should substract moneys with same currency"
+  it "should multiply moneys with same currency"
+  it "should divide moneys with same currency"
+
+  it "should add moneys with different currencies"
+  it "should substract moneys with different currencies"
+  it "should multiply moneys with different currencies"
+  it "should divide moneys with different currencies"
+
+  it "should compare equality of moneys with same currency"
+  it "should compare equality of moneys with different currencies"
+  it "should consider two monetary amounts as equal if they agree up to the cents"
+
+  it "should test greater than for moneys with same currency"
+  it "should test less than for moneys with same currency"
+
+  it "should test greater than for moneys with different currencies"
+  it "should test less than for moneys with different currencies"
 end
